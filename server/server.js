6eb317407cad1d33106a8f9861177cc97204aa2f
abdrafay <zainrafay12@gmail.com>
2022-05-08 03:53:39 +0500

    Server Chnages

--- server/server.js
+++ server/server.js
@@ -27,10 +27,8 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-
 app.use(notFound)
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
