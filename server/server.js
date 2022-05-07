871c42f3cf6032a82fb2e4b168ae2cbf86a240b7
abdrafay <zainrafay12@gmail.com>
2022-05-08 03:45:31 +0500

    Server Chnages

--- server/server.js
+++ server/server.js
@@ -13,8 +13,7 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/projects', require('./routes/projectRoutes'), require('./routes/taskRoutes'))
 app.use('/api/positions', require('./routes/disPosRoutes'))
 app.use('/api/roles', require('./routes/roleRoutes'))
-app.use(notFound)
-app.use(errorHandler)
+
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
@@ -27,6 +26,11 @@ if(process.env.NODE_ENV === 'production'){
         res.send('Server is running')
     })
 }
+
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
